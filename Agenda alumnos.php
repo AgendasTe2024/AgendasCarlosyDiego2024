b31ff369d0daffe1d310538c080693efbc8f261d
AgendasTe2024 <162557996+AgendasTe2024@users.noreply.github.com>
2024-03-07 02:47:25 -0600

    Add files via upload

---
+++ Agenda alumnos.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<title>Clinica Tepepan</title>
+<link rel="shortcut icon" href="higiene.png">
+
+</head>
+  
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <LINK REL=StyleSheet href="Conexion/estilo.css" TYPE="text/css" MEDIA=screen>
+
+  <!--    Datatables  -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css" />
+
+  <body style="background-color: black; color: white;">
+  <center>
+  <img src="l1.jpg" height="40%" width="100%">
+  </center>
+
+<ol class="breadcrumb" style="position:relative; left: 25%; width:70%; background-color:black;">
+  <li class="breadcrumb-item"><a href="index.html">INICIO</a></li>
+  <li class="breadcrumb-item"><a href="Agenda adultos.php">AGENDA ADULTOS</a></li>
+  <li class="breadcrumb-item "><a href="Agenda niños.php">AGENDA NIÑOS</a></li>
+  <li class="breadcrumb-item "><a href="Agenda alumnos.php">AGENDA ALUMNOS</a></li>
+  <li class="breadcrumb-item "><a href="Agenda sesion.php">AGENDA DE SESION</a></li>
+  <li class="breadcrumb-item "><a href="Administrador.php">ADMINSTRADOR</a></li>
+</ol>
+
+<center><h1><p> AGENDA DE ACTIVIDADES ACADEMICAS INTERNAS ESTUDIANTILES</p></h1></center>
+
+<center><h2><p> REVISIÓN DE EXPDIENTES</p></h2></center>
+<center><table border="1" width="90%" bgcolor"fff7db"></center>
+<tr>
+<td>Los expedientes se revisarán exclusivamente en la semana 2 y en la semana 4</td>
+</tr>
+</table>
+
+
+<center><h2><p> PONENCIAS</p></h2></center>
+<center><table border="1" width="90%" bgcolor"fff7db"></center>
+<tr>
+<td>LUNES 15 DE NOV</td>
+<td>DRA MONICA/ PROVISIONALES</td>
+</tr>
+<tr>
+<td>MARTES 25 DE NOV</td>
+<td>DRA JESSICA/ PULPECTOMIA</td>
+</tr>
+</table>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
+
+
+  <!--    Datatables-->
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>
+
+
+</body>
+
+</html>
